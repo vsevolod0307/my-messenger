60e0a968fbf2e5c9f9bfaae593099a07fe1132a3
Vsevolod <seven@acceptsib.ru>
2023-02-27 01:39:53 +0700

    add change data profile

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,9 +1,39 @@
 <template>
     <div class="profile">
-        <div class="user-avatar">
-            <input type="file" @change="editImage">
+        <div class="profile-info" v-if="!isEdit">
+            <div class="avatar">
+                <img :src="avatarImage" alt="">
+            </div>
+            <span class="first-name">{{ first_name }}</span>
+            <span class="last-name">{{ last_name }}</span>
+            <br>
+            <div class="age">{{ 'Возраст:' + age }}</div>
+            <div class="age">{{ 'Пол:' + gender }}</div>
+            <div class="about">
+                {{ 'О себе:' + about_us }}
+            </div>
+        </div>
+        <div class="profile-edit" v-if="isEdit">
+            <div class="user-avatar">
+                <div class="avatar">
+                    <img :src="avatarImage" alt="">
+                </div>
+                <input type="file" @change="editImage">
+            </div>
+            <input type="text" v-model="first_name" placeholder="Введите Имя" required>
+            <input type="text" v-model="last_name" placeholder="Введите Фамилию" required>
+            <input type="number" v-model="age" placeholder="Ваш возраст" required>
+            <div class="gender">
+                <label for="gender">Мужской</label>
+                <input type="radio" name="gender" v-model="gender" value="male">
+                <label for="gender">Женский</label>
+                <input type="radio" name="gender" v-model="gender" value="female">
+            </div>
+            <textarea class="about_us" v-model="about_us" cols="30" rows="5" placeholder="Напишите о себе"></textarea>
+            <button @click="writeUserData" class="save-changes">Сохранить</button>
         </div>
         <div class="sign-out">
+            <button @click="isEdit = true">Редактировать</button>
             <button @click="signOutUser">Выйти</button>
         </div>
     </div>
@@ -11,33 +41,74 @@
 
 <script>
 import { signOut, getAuth } from 'firebase/auth';
-import { getDatabase, set, ref } from "firebase/database";
+import { getDatabase, set, ref, onValue, get, child } from "firebase/database";
+import { getStorage, ref as refStor, uploadBytes, getDownloadURL } from 'firebase/storage';
 
 export default {
     data() {
         return {
             auth: getAuth(),
-            id: this.$route.params.id,
-            imageUrl: ""
+            userUid: this.$route.params.id,
+            avatarImage: "",
+            first_name: "",
+            last_name: "",
+            age: "",
+            gender: "",
+            about_us: "",
+            isEdit: false
         }
     },
     methods: {
         signOutUser() {
             signOut(this.auth);
         },
-        editImage(e) {
-            console.log(e.target.value);
+        async editImage(imageUrl) {
+            const storage = getStorage();
+            const storRef = refStor(storage, "avatar")
+            await uploadBytes(storRef, imageUrl.target.files[0]).then((info) => {
+                console.log(info.metadata)
+            })
+            await getDownloadURL(refStor(storage, "avatar"))
+            .then((url) => this.avatarImage = url)
         },
-        writeUserData(userId, name, email, imageUrl) {
+        writeUserData() {
             const db = getDatabase();
-            set(ref(db, 'users/' + userId), {
-                username: name,
-                email: email,
-                profile_picture: imageUrl
+            set(ref(db, `users/(${this.userUid})`), {
+                first_name: this.first_name,
+                last_name: this.last_name,
+                age: this.age,
+                gender: this.gender,
+                about_us: this.about_us
+            })
+            .then(s => console.log(s))
+            .catch((error) => {
+                console.log(error)
             });
-        }
+            this.isEdit = false;
+        },
+        // getDataProfile() {
+        //     const dbRef = ref(getDatabase());
+        //     get(child(dbRef, this.userUid))
+        //     .then((snapshot) => {
+        //         if (snapshot.exists()) {
+        //             console.log(snapshot.val());
+        //         } else {
+        //             console.log("No data available");
+        //         }
+        //         }).catch((error) => {
+        //         console.error(error);
+        //         });
+        //     // const db = getDatabase();
+        //     // const profileInfo = ref(db, this.userUid);
+        //     // onValue(profileInfo, (snapshot) => {
+        //     //     const data = snapshot.val();
+        //     //     console.log(data);
+        //     //     console.log(snapshot.exists());
+        //     // })
+        // }
     },
     mounted() {
+        // this.getDataProfile();
         // this.writeUserData("88", "test", "kkk@mail.ru", "image.png");
     }
 }
@@ -61,5 +132,19 @@ export default {
     }
     .sign-out button:hover {
         background-color: rgba(245, 64, 68, 0.7);
+    }
+    .profile {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 20px;
+    }
+
+    .profile > input {
+        padding: 10px;
+        font-size: 16px;
+    }
+    .about_us {
+        resize: none;
     }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -16,11 +16,11 @@ const firebaseApp = initializeApp({
     apiKey: "AIzaSyAINxc4F8ZbKRUdv8L77VPVEArtxYLxVXw",
     authDomain: "my-messenger-82417.firebaseapp.com",
     projectId: "my-messenger-82417",
-    storageBucket: "my-messenger-82417.appspot.com",
+    storageBucket: "gs://my-messenger-82417.appspot.com",
     messagingSenderId: "960604895089",
     appId: "1:960604895089:web:956602d402407c2c87a8d0",
     measurementId: "G-HZREB0MVT4",
-    databaseURL: "https://my-messenger-82417-default-rtdb.firebaseio.com"
+    databaseURL: "https://my-messenger-82417-default-rtdb.firebaseio.com",
 })
 
 createApp(App).use(store).use(router)
