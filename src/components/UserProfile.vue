1ce903818fe814202e165bf81ca1a69c2c0419b7
Vsevolod <seven@acceptsib.ru>
2023-02-28 00:05:37 +0700

    connection with database updated

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -2,34 +2,34 @@
     <div class="profile">
         <div class="profile-info" v-if="!isEdit">
             <div class="avatar">
-                <img :src="avatarImage" alt="">
+                <img :src="dataProfile.avatarImage" alt="">
             </div>
-            <span class="first-name">{{ first_name }}</span>
-            <span class="last-name">{{ last_name }}</span>
+            <span class="first-name">{{ dataProfile.first_name }}</span>
+            <span class="last-name">{{ dataProfile.last_name }}</span>
             <br>
-            <div class="age">{{ 'Возраст:' + age }}</div>
-            <div class="age">{{ 'Пол:' + gender }}</div>
+            <div class="age">{{ 'Возраст:' + dataProfile.age }}</div>
+            <div class="age">{{ 'Пол:' + dataProfile.gender }}</div>
             <div class="about">
-                {{ 'О себе:' + about_us }}
+                {{ 'О себе:' + dataProfile.about_us }}
             </div>
         </div>
         <div class="profile-edit" v-if="isEdit">
             <div class="user-avatar">
                 <div class="avatar">
-                    <img :src="avatarImage" alt="">
+                    <img :src="dataProfile.avatarImage" alt="">
                 </div>
                 <input type="file" @change="editImage">
             </div>
-            <input type="text" v-model="first_name" placeholder="Введите Имя" required>
-            <input type="text" v-model="last_name" placeholder="Введите Фамилию" required>
-            <input type="number" v-model="age" placeholder="Ваш возраст" required>
+            <input type="text" v-model="dataProfile.first_name" placeholder="Введите Имя" required>
+            <input type="text" v-model="dataProfile.last_name" placeholder="Введите Фамилию" required>
+            <input type="number" v-model="dataProfile.age" placeholder="Ваш возраст" required>
             <div class="gender">
                 <label for="gender">Мужской</label>
-                <input type="radio" name="gender" v-model="gender" value="male">
+                <input type="radio" name="gender" v-model="dataProfile.gender" value="male">
                 <label for="gender">Женский</label>
-                <input type="radio" name="gender" v-model="gender" value="female">
+                <input type="radio" name="gender" v-model="dataProfile.gender" value="female">
             </div>
-            <textarea class="about_us" v-model="about_us" cols="30" rows="5" placeholder="Напишите о себе"></textarea>
+            <textarea class="about_us" v-model="dataProfile.about_us" cols="30" rows="5" placeholder="Напишите о себе"></textarea>
             <button @click="writeUserData" class="save-changes">Сохранить</button>
         </div>
         <div class="sign-out">
@@ -48,14 +48,16 @@ export default {
     data() {
         return {
             auth: getAuth(),
-            userUid: this.$route.params.id,
-            avatarImage: "",
-            first_name: "",
-            last_name: "",
-            age: "",
-            gender: "",
-            about_us: "",
-            isEdit: false
+            sRef: ref(getDatabase(), `users/(${this.$route.params.id})`),
+            dataProfile: {
+                avatarImage: "",
+                first_name: "",
+                last_name: "",
+                age: "",
+                gender: "",
+                about_us: "",
+            },
+            isEdit: false,
         }
     },
     methods: {
@@ -64,43 +66,37 @@ export default {
         },
         async editImage(imageUrl) {
             const storage = getStorage();
-            const storRef = refStor(storage, "avatar")
-            await uploadBytes(storRef, imageUrl.target.files[0]).then((info) => {
-                console.log(info.metadata)
-            })
+            const storRef = refStor(storage, "avatar");
+            await uploadBytes(storRef, imageUrl.target.files[0]);
             await getDownloadURL(refStor(storage, "avatar"))
-            .then((url) => this.avatarImage = url)
+                .then((url) => this.dataProfile.avatarImage = url)
         },
         writeUserData() {
-            const db = getDatabase();
-            set(ref(db, `users/(${this.userUid})`), {
-                first_name: this.first_name,
-                last_name: this.last_name,
-                age: this.age,
-                gender: this.gender,
-                about_us: this.about_us
+            set(this.sRef, {
+                first_name: this.dataProfile.first_name,
+                last_name: this.dataProfile.last_name,
+                age: this.dataProfile.age,
+                gender: this.dataProfile.gender,
+                about_us: this.dataProfile.about_us
+            })
+            .then(() => {
+                this.updateDataProfile();
             })
             this.isEdit = false;
         },
-        getDataProfile() {
-            const storage = getDatabase();
-            const sRef = ref(storage, `users/(TUvBqw8YMeOp9dRsTlG5oZBvA8L2)`)
-            console.log(sRef.key)
-            onValue(sRef, data => {
-                console.log(data.val())
+        updateDataProfile() {
+            onValue(this.sRef, data => {
+                if(data.exists()) {
+                    this.dataProfile = data.val();
+                } else {
+                    this.isEdit = true;
+                }
             })
-            // const db = getDatabase();
-            // const profileInfo = ref(db, this.userUid);
-            // onValue(profileInfo, (snapshot) => {
-            //     const data = snapshot.val();
-            //     console.log(data);
-            //     console.log(snapshot.exists());
-            // })
         }
     },
-    created() {
-        this.getDataProfile();
-        // this.writeUserData("88", "test", "kkk@mail.ru", "image.png");
+    async created() {
+        await this.updateDataProfile();
+        console.log(this.dataProfile);
     }
 }
 </script>
